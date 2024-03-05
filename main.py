4508161cf9f00f3536c50e9bcc021b2ee48b0063
Andrea <andrea.walter03@gmail.com>
2024-03-05 13:23:22 +0100

    all 3 heuristics done, project should be finished

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sys
 import argparse
+from time import time
 from solvability import isSolvable
-from solver import solv
+from solver import solv, findCoordinates, getScore
 
 
-def make_goal(s):
+class bcolors:
+	GREEN = '\033[92m'
+	BLUE = '\033[94m'
+	PURPLE = '\033[95m'
+	ORANGE = '\033[93m'
+	ENDC = '\033[0m'
+
+
+def makeGoal(s):
 	ts = s*s
 	puzzle = [-1 for i in range(ts)]
 	cur = 1
@@ -31,7 +40,7 @@ def make_goal(s):
 	return puzzle
 
 
-def extract_puzzle(original: str)->list:
+def extractPuzzle(original: str)->list:
 	size = -1
 
 	tab = original.split('\n')
@@ -51,27 +60,71 @@ def extract_puzzle(original: str)->list:
 		if len(puzzle[it]) <= 2:
 			puzzle.pop(it)
 		elif len(puzzle[it]) != size:
-			print("problem")
+			print("there is a problem in the puzzle input")
 			exit()
 		else:
 			it += 1
+	if (len(puzzle) != size):
+		print("there is a problem in the puzzle input")
+		exit()
+	flatten = [j for sub in puzzle for j in sub]
+	for i in range (size*size):
+		if str(i) not in flatten:
+			print(f'{i} not in input')
+			exit()
 	return [puzzle, size]
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument("file", type=str)
+	parser.add_argument('file', type=str)
+	parser.add_argument("-m", "--manhattan", action="store_true", default=False, help='manhattan distance')
+	parser.add_argument("-H", "--hamming", action="store_true", default=False, help='hamming distance')
+	parser.add_argument("-l", "--linear", action="store_true", default=False, help='manhattan distance with linear conflicts')
 	args = parser.parse_args()
 
-	f = open(args.file, "r")
-	original = f.read()
-
-	puzzle, size = extract_puzzle(original)
-	goal = make_goal(size)
-
-	if (not isSolvable(puzzle, goal, size)):
-		print ("puzzle is unsolvable")
+	if (args.manhattan and args.hamming) or (args.manhattan and args.linear) or (args.linear and args.hamming):
+		print('cannot execute 2 or more heuristics at the same time')
 		exit()
 
-	print(f"!!!!!!!{solv(puzzle, size, goal)}")
+	heuristic = 0
+	if args.hamming:
+		heuristic = 1
+	if args.linear:
+		heuristic = 2
 
+	f = open(args.file, 'r')
+	original = f.read()
+
+	puzzle, size = extractPuzzle(original)
+	goal = makeGoal(size)
+
+	if (not isSolvable(puzzle, goal, size)):
+		print('puzzle is unsolvable')
+		exit()
+
+	if getScore(puzzle, goal, size, heuristic) == 0:
+		print('puzzle is already solved')
+		exit()
+
+	start = time()
+	moves, space, selection = solv(puzzle, size, goal, heuristic)
+	end = time()
+
+	print (f"Solving the puzzle took {bcolors.GREEN}{end - start}{bcolors.ENDC} seconds")
+	print (f"The cost was {bcolors.BLUE}{len(moves)}{bcolors.ENDC} moves\n")
+	print (f"{bcolors.PURPLE}{selection}{bcolors.ENDC} states were selected in the opened set")
+	print (f"{bcolors.ORANGE}{space}{bcolors.ENDC} states were represented in the memory at the same time\n")
+
+	for l in puzzle:
+		print(' '.join(l))
+	print()
+
+	for m in moves:
+		x1, y1 = findCoordinates(puzzle, size, '0')
+		x2, y2 = findCoordinates(puzzle, size, m)
+		puzzle[y1][x1], puzzle[y2][x2] = puzzle[y2][x2], puzzle[y1][x1]
+
+		for l in puzzle:
+			print(' '.join(l))
+		print()
--- solver.py
+++ solver.py
@@ -1,7 +1,10 @@
-def find_coordinates(puzzle: list, size: int):
+#https://theory.stanford.edu/~amitp/GameProgramming/Variations.html
+#https://en.wikipedia.org/wiki/A*_search_algorithm
+
+def findCoordinates(puzzle: list, size: int, n: str):
 	for y in range(size):
 		for x in range(size):
-			if puzzle[y][x] == '0':
+			if puzzle[y][x] == n:
 				return (x, y)
 
 
@@ -13,51 +16,107 @@ def manhattanDistance(flatten: list, goal: list, size: int, element: int):
 	return abs(p1 - q1) + abs(p2 - q2)
 
 
-def getScore(puzzle: list, goal: list, size: int):
+def	misplacedTiles(flatten: list, goal: list):
 	score = 0
-	flatten = [j for sub in puzzle for j in sub]
-	for i in range(size**2):
-		score += manhattanDistance(flatten, goal, size, i)
+	for i, t in enumerate(flatten):
+		if t != str(goal[i]):
+			score += 1
 	return score
 
 
-def move_up(state: list, size: int, x: int, y: int):
+def	linearConflict(flatten: list, puzzle: list, goal: list, size: list):
+	nb_conflict = 0
+	built_goal = [goal[size*i:size*(i+1)] for i in range(size + 1)]
+
+	for y, l in enumerate(puzzle):
+		for i, e in enumerate(l):
+			try:
+				goal_index = built_goal[y].index(int(e))
+				if goal_index > i:
+					for element in l[i:goal_index + 1]:
+						try:
+							ele_index = built_goal[y].index(int(element))
+							if ele_index <= i:
+								nb_conflict += 1
+						except:
+							pass
+			except:
+				pass
+
+	r_puzzle = list(zip(*puzzle[::-1]))
+	r_goal = list(zip(*built_goal[::-1]))
+
+	for y, l in enumerate(r_puzzle):
+		for i, e in enumerate(l):
+			try:
+				goal_index = r_goal[y].index(int(e))
+				if goal_index > i:
+					for element in l[i:goal_index + 1]:
+						try:
+							ele_index = r_goal[y].index(int(element))
+							if ele_index <= i:
+								nb_conflict += 1
+						except:
+							pass
+			except:
+				pass
+
+	return (nb_conflict * 2)
+
+			
+def getScore(puzzle: list, goal: list, size: int, heuristic: int):
+	score = 0
+	flatten = [j for sub in puzzle for j in sub]
+	if heuristic == 0:
+		for i in range(size**2):
+			score += manhattanDistance(flatten, goal, size, i)
+
+	elif heuristic == 1:
+		score = misplacedTiles(flatten, goal)
+
+	elif heuristic == 2:
+		for i in range(size**2):
+			score += manhattanDistance(flatten, goal, size, i)
+		score += linearConflict(flatten, puzzle, goal, size)
+
+	return score
+
+
+def moveUp(state: list, size: int, x: int, y: int):
 	temp = [l[:] for l in state]
 	act = temp[y - 1][x]
 	temp[y][x], temp[y - 1][x] = temp[y - 1][x], temp[y][x]
 	return temp, act
 
 
-def move_down(state: list, size: int, x: int, y: int):
+def moveDown(state: list, size: int, x: int, y: int):
 	temp = [l[:] for l in state]
 	act = temp[y + 1][x]
 	temp[y][x], temp[y + 1][x] = temp[y + 1][x], temp[y][x]
 	return temp, act
 
 
-def move_left(state: list, size: int, x: int, y: int):
+def moveLeft(state: list, size: int, x: int, y: int):
 	temp = [l[:] for l in state]
 	act = temp[y][x - 1]
 	temp[y][x], temp[y][x - 1] = temp[y][x - 1], temp[y][x]
-	# print (temp)
-	# print (state)
 	return temp, act
 
 
-def move_right(state: list, size: int, x: int, y: int):
+def moveRight(state: list, size: int, x: int, y: int):
 	temp = [l[:] for l in state]
 	act = temp[y][x + 1]
 	temp[y][x], temp[y][x + 1] = temp[y][x + 1], temp[y][x]
 	return temp, act
 
 
-def insertState(temp: list, goal: list, size: int, states: list, all_states: list, moves: list, scores: int, action: str, copy: list):
+def insertState(temp: list, goal: list, size: int, states: list, all_states: list, moves: list, scores: int, action: str, copy: list, heuristic: int):
 	if temp in all_states:
 		return states, moves, scores
 	index = states.index(copy)
 	new_move = moves[index][:]
 	new_move.append(action)
-	new_score = getScore(temp, goal, size) * (size-2) + len(new_move)
+	new_score = getScore(temp, goal, size, heuristic) * (size-2) + len(new_move)
 	for i, s in enumerate(scores):
 		if new_score <= s:
 			states.insert(i, temp)
@@ -71,54 +130,52 @@ def insertState(temp: list, goal: list, size: int, states: list, all_states: lis
 	states.append(temp)
 	moves.append(new_move)
 	scores.append(new_score)
-	if (len(scores) >= size * 10):
-		states.pop(-1)
-		moves.pop(-1)
-		scores.pop(-1)
 	return states, moves, scores
 
 
 
-def solv(puzzle: list, size: int, goal: list) -> list:
+def solv(puzzle: list, size: int, goal: list, heuristic: int) -> list:
 	states = [puzzle, ]
 	all_states = [puzzle, ]
 	moves = [[]]
-	scores = [getScore(puzzle, goal, size) * (size-2), ]
-
-	good = False
+	scores = [getScore(puzzle, goal, size, heuristic) * (size-2), ]
 	action = ''
+	selection = 0
 
 
-	# t = 0
 	while (True):
+		selection += 1
 		copy = [l[:] for l in states[0]]
-		x, y = find_coordinates(states[0], size)
+		x, y = findCoordinates(states[0], size, '0')
 		if (y > 0):
 			index = states.index(copy)
-			temp, action = move_up(states[index], size, x, y)
-			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy)
+			temp, action = moveUp(states[index], size, x, y)
+			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy, heuristic)
 			all_states.append([l[:] for l in temp])
 		if (y < size - 1):
 			index = states.index(copy)
-			temp, action = move_down(states[index], size, x, y)
-			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy)
+			temp, action = moveDown(states[index], size, x, y)
+			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy, heuristic)
 			all_states.append([l[:] for l in temp])
 		if (x > 0):
 			index = states.index(copy)
-			temp, action = move_left(states[index], size, x, y)
-			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy)
+			temp, action = moveLeft(states[index], size, x, y)
+			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy, heuristic)
 			all_states.append([l[:] for l in temp])
 		if (x < size - 1):
 			index = states.index(copy)
-			temp, action = move_right(states[index], size, x, y)
-			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy)
+			temp, action = moveRight(states[index], size, x, y)
+			states, moves, scores = insertState(temp, goal, size, states, all_states, moves, scores, action, copy, heuristic)
 			all_states.append([l[:] for l in temp])
+
 		index = states.index(copy)
 		states.pop(index)
 		moves.pop(index)
 		scores.pop(index)
+
+		states[:size*10]
+		moves[:size*10]
+		scores[:size*10]
+
 		if scores[0] - len(moves[0]) == 0:
-			print(len(scores))
-			print (states[0])
-			return moves[0]
-		# print((scores[0] - len(moves[0])) // 2)
+			return moves[0], len(all_states), selection
